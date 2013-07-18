2947c354fb46273c87d624c6744cecfeb1abbc21
Troels Abrahamsen <troels@leftywork.dk>
2013-07-18 13:41:11 +0200

    Removed mandatory inclusion of jquery

--- base/engine/hooks.php
+++ base/engine/hooks.php
@@ -1,20 +1,4 @@
 <?
 
-/**
- * Initiates the boilerplate with the settings
- * defined in init.php in the site/ folder
- *
- * @author Troels Abrahamsen
- * @param $param
- * @return return type
-*/
-
-function boilerplate_init(){
-	global $settings;
-	if($settings->jquery){
-		wp_enqueue_script('jquery');
-	}
-}
-add_action('wp_head', 'boilerplate_init', 0);
 
 ?>
