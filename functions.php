ae31e2024afaa271de0c66061141eaebeda94c16
Troels Abrahamsen <troels@leftywork.dk>
2013-07-18 14:04:13 +0200

    functions file now initiates the rigs object

--- functions.php
+++ functions.php
@@ -1,6 +1,24 @@
 <?
+
+/* !THIS SHOULD BE DYNAMIC IN TIME */
+error_reporting(E_WARNING | E_ERROR );
+ini_set('display_errors', 'On');
+
+/*
+Include the main engine file, the rigs class file
+and then make a new rigs object called...you guessed it...$rigs!
+*/
 require('base/engine/main.php');
-if($file = get_function('main')){
+require(get_engine('rigs.class'));
+$rigs = new rigs();
+
+/*
+Call the main engine file.
+That file should require all other engine files.
+*/
+if($file = get_engine('main')){
 	require($file);
-}	
+}
+unset($file);
+
 ?>
