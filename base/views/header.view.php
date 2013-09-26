b986fd5cafb3ae79eadb6f314e5bc481985ff441
supertroels <troels@troelss-imac.home>
2013-09-26 11:16:43 +0200

    New version of mostly all files

--- base/views/header.view.php
+++ base/views/header.view.php
@@ -1,7 +1,23 @@
 <!DOCTYPE html>
-<html>
+
+<!--[if lt IE 9]><html class="ie"><![endif]-->
+<!--[if gte IE 9]><!--><html><!--<![endif]-->
+
 <head>
 	<title><? bloginfo('name') ?></title>
+	
+	<meta charset="<? bloginfo('charset') ?>"/>
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+	<? if($rigs->responsive): ?>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<? endif; ?>
+
+	<!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
 	<? wp_head() ?>
 </head>
 <body <? body_class() ?>>
--- index.php
+++ index.php
@@ -1,75 +1,27 @@
 <?
-if(is_front_page() and $file = get_view('front-page')){
-	if($worker = get_worker('front-page')){
-	}
-}
-elseif(is_404() and $file = get_view('404')){
-	if($worker = get_worker('404')){
-	}
-}
-elseif(is_single() or is_page()){
-	if(is_page() and $file = get_view('page-'.$post->post_name)){
-		if($worker = get_worker('page-'.$post->post_name)){
-		}
-	}
-	elseif(is_page() and $file = get_view('page')){
-		if($worker = get_worker('page')){
-		}
-	}
-	elseif($file = get_view('single-'.get_post_type())){
-		if($worker = get_worker('single-'.get_post_type())){
-		}
-	}
-	elseif($file = get_view('single')){
-		if($worker = get_worker('single')){
-		}
-	}
-}
-elseif(is_archive()){
-
-	$post_type = get_query_var('post_type');
-
-	if($file = get_view('archive-'.$post_type)){
-		if($worker = get_worker('archive-'.$post_type)){
-		}
-	}
-	elseif($file = get_view('archive')){
-		if($worker = get_worker('archive')){
-		}
-	}
-}
-else{
-	if($file = get_view('index')){
-		if($worker = get_worker('index')){
-		}
-	}
-	else{
-		die('You need an index file');
-	}
-}
-
-
+/* Set sails :) */
+$rigs->sails();
 
 /* First require the worker... */
-if($worker){
-	require($worker);
+if($rigs->worker){
+	require($rigs->worker);
 }
 
 /* ...then the WP header. */
 get_header();
 
 /* If a custom head.view.php file exists, require that now. */
-if($head = get_view('head')){
+if($head = get_view($rigs->head)){
 	require($head);
 }
 
 /* Require the main view file */
-if($file){
-	require($file);
+if($rigs->view){
+	require($rigs->view);
 }
 
 /* Require any custom foot.view.php */
-if($foot = get_view('foot')){
+if($foot = get_view($rigs->foot)){
 	require($foot);
 }
 
