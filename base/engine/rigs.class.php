d15233aa21dbf7314f949d9f2d5fe0e6f2c8cc23
supertroels <troels@troelss-imac.home>
2013-09-30 12:15:08 +0200

    Now supports search archive tempate

--- base/engine/rigs.class.php
+++ base/engine/rigs.class.php
@@ -47,34 +47,32 @@ class rigs {
 
 		if(is_front_page() and $this->view = get_view('front-page')){
 			$this->page = 'front-page';
-			if($this->worker = get_worker('front-page')){
-			}
+			$this->worker = get_worker('front-page'));
 		}
 		elseif(is_404() and $this->view = get_view('404')){
 			$this->page = '404';
-			if($this->worker = get_worker('404')){
-			}
+			$this->worker = get_worker('404'));
+		}
+		elseif(is_search() and $this->view = get_view('search')){
+			$this->page = 'search';
+			$this->worker = get_worker('search'));
 		}
 		elseif(is_single() or is_page()){
 			if(is_page() and $this->view = get_view('page-'.$post->post_name)){
 				$this->page = 'page-'.$post->post_name;
-				if($this->worker = get_worker('page-'.$post->post_name)){
-				}
+				$this->worker = get_worker('page-'.$post->post_name));
 			}
 			elseif(is_page() and $this->view = get_view('page')){
 				$this->page = 'page';
-				if($this->worker = get_worker('page')){
-				}
+				$this->worker = get_worker('page'));
 			}
 			elseif($this->view = get_view('single-'.get_post_type())){
 				$this->page = 'single-'.get_post_type();
-				if($this->worker = get_worker('single-'.get_post_type())){
-				}
+				$this->worker = get_worker('single-'.get_post_type()));
 			}
 			elseif($this->view = get_view('single')){
 				$this->page = 'single';
-				if($this->worker = get_worker('single')){
-				}
+				$this->worker = get_worker('single'));
 			}
 		}
 		elseif(is_archive()){
@@ -83,20 +81,17 @@ class rigs {
 
 			if($this->view = get_view('archive-'.$post_type)){
 				$this->page = 'archive-'.$post_type;
-				if($this->worker = get_worker('archive-'.$post_type)){
-				}
+				$this->worker = get_worker('archive-'.$post_type));
 			}
 			elseif($this->view = get_view('archive')){
 				$this->page = 'archive';
-				if($this->worker = get_worker('archive')){
-				}
+				$this->worker = get_worker('archive'));
 			}
 		}
 		else{
 			if($this->view = get_view('index')){
 				$this->page = 'index';
-				if($this->worker = get_worker('index')){
-				}
+				$this->worker = get_worker('index'));
 			}
 			else{
 				die('You need an index file');
